e9c21492d92a0249adaca549da87ebc65302ca98
Mrsenior12 <61933232+Mrsenior12@users.noreply.github.com>
2020-05-15 13:00:36 +0200

    particle

--- particle.py
+++ particle.py
@@ -1,1 +1,29 @@
-print("tak")
+class Particle:
+    def __init__(self,masa,wspol_X,wspol_Y,promien,pred_poz,pred_pion):
+        self.masa = 10
+        self.wspol_X = wspol_X
+        self.wspol_Y = wspol_Y
+        self.promien = promien
+        self.pred_poz = pred_poz
+        self.pred_pion = pred_pion
+
+    def dodaj(ilosc):
+        for i in range(ilosc):
+            wspol_X = int(input("podaj wspol X: "))
+            wspol_Y = int(input("podaj wspol Y: "))
+            promien = int(input("podaj promien: "))
+            pred_poz = int(input("podaj predkosc w poziomie: "))
+            pred_pion = int(input("podaj predkosc w pinie: "))
+            czasteczki.append(Particle(wspol_X, wspol_Y, promien, pred_poz, pred_pion))
+#def dodaj(wspol_X,wspol_Y,promien,pred_poz,pred_pion):
+czasteczki=[]
+ilosc = int(input("podaj ilosc czasteczek: "))
+for i in czasteczki:
+    print(i.wspol_X,i.wspol_Y,i.promien,i.pred_pion,i.pred_poz)
+#tak wygląda przykładowa zamiana
+for i in range(2):
+    x = int(input("podaj wartosc X: "))
+    czasteczki[i].wspol_X = x
+
+for i in czasteczki:
+    print(i.wspol_X,i.wspol_Y,i.promien,i.pred_pion,i.pred_poz)
