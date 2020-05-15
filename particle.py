f992903e2939f6857dc4deb4e510f7cc04f62e13
V-Ader <56037641+V-Ader@users.noreply.github.com>
2020-05-15 16:20:27 +0200

    collisions moved

--- particle.py
+++ particle.py
@@ -1,6 +1,8 @@
 import random
+import Mmth
+
 class Particle:
-    czasteczki = []
+    Particles = []
     def __init__(self,masa,x,y,radius,vel_x,vel_y):
         self.masa = 10
         self.x = x
@@ -8,14 +10,22 @@ class Particle:
         self.radius = radius
         self.vel_x = vel_x
         self.vel_y = vel_y
-    def dodaj(self,ilosc):
+    def add(self,ilosc):
         for i in range(ilosc):
             x = random.randrange(400)
             y = random.randrange(400)
             radius = 5
             vel_x = 5
             vel_y = 5
-            czasteczki.append(Particle(x,y,radius,vel_x,vel_y))
+            Particle.append(Particle(x,y,radius,vel_x,vel_y))
+
+def collide(p1, p2):
+    d = p1.radius + p2.radius
+
+    normal_x = math.abs((p2.x - p1.x) / d)
+    normal_y = math.abs((p2.y - p1.y) / d)
+
+
 
 #ilosc = int(input("podaj ilosc czasteczek: "))
 #for i in czasteczki:
--- physics.py
+++
@@ -1,5 +0,0 @@
-from particle import*
-
-def collision(p1, p2): #particles as parameters
-    p1.vel_v , p2.vel_v = p2.vel_v , p1.vel_v
-    p1.vel_h , p2.vel_h = p2.vel_h , p1.vel_h
