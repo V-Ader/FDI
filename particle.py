96695e9f1ae9f2bb9dd9aaa95d45d1ea3bd9f61f
Mrsenior12 <61933232+Mrsenior12@users.noreply.github.com>
2020-05-15 13:15:35 +0200

    poprawka

--- particle.py
+++ particle.py
@@ -1,4 +1,6 @@
+import random
 class Particle:
+    czasteczki = []
     def __init__(self,masa,wspol_X,wspol_Y,promien,pred_poz,pred_pion):
         self.masa = 10
         self.wspol_X = wspol_X
@@ -6,17 +8,15 @@ class Particle:
         self.promien = promien
         self.pred_poz = pred_poz
         self.pred_pion = pred_pion
-
-    def dodaj(ilosc):
+    def dodaj(self,ilosc):
         for i in range(ilosc):
-            wspol_X = int(input("podaj wspol X: "))
-            wspol_Y = int(input("podaj wspol Y: "))
-            promien = int(input("podaj promien: "))
-            pred_poz = int(input("podaj predkosc w poziomie: "))
-            pred_pion = int(input("podaj predkosc w pinie: "))
+            wspol_X = random.randrange(400)
+            wspol_Y = random.randrange(400)
+            promien = 5
+            pred_poz = 5
+            pred_pion = 5
             czasteczki.append(Particle(wspol_X, wspol_Y, promien, pred_poz, pred_pion))
-#def dodaj(wspol_X,wspol_Y,promien,pred_poz,pred_pion):
-czasteczki=[]
+
 ilosc = int(input("podaj ilosc czasteczek: "))
 for i in czasteczki:
     print(i.wspol_X,i.wspol_Y,i.promien,i.pred_pion,i.pred_poz)
